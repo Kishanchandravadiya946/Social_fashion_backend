628163458c96c675f69209e16f00e5535eba010d
vandit <vanditmuniya@gmail.com>
2025-02-26 17:43:30 +0530

    user profile done.

--- config.py
+++ config.py
@@ -16,6 +16,6 @@ class Config:
     JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY",'default_secret_key')
     JWT_TOKEN_LOCATION=["headers"] 
     JWT_IDENTITY_CLAIM="user_id"
-    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=10)
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=60)
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
