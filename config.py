781040a50fe32ff3eb366679b8b0866c49c6a949
Kishan chandravadiya <kishanchandravadiya946@gmail.com>
2025-05-09 16:07:34 +0530

    NEON db

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_AZURE')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI_NEON')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv(
         'SQLALCHEMY_TRACK_MODIFICATIONS', 'False').lower() == 'true'
 
