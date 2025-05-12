978a8c67e57dde31a09d5d208cb53d8a22a97c65
Kishan chandravadiya <kishanchandravadiya946@gmail.com>
2025-05-12 18:19:39 +0530

    cors solve

--- app.py
+++ app.py
@@ -4,38 +4,41 @@ from extensions import db
 from flask_migrate import Migrate
 from models import init_models
 from api.user.views import user_bp
-from api.product.views import product_category_bp,product_bp,product_item_bp,variation_bp,variation_option_bp
-from api.shopping_cart.view import shopping_cart_item,wishlist_bp
+from api.product.views import product_category_bp, product_bp, product_item_bp, variation_bp, variation_option_bp
+from api.shopping_cart.view import shopping_cart_item, wishlist_bp
 from api.order.view import shop_order
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from api.user.resources.user_resource import revoked_tokens
-app=Flask(__name__)
+app = Flask(__name__)
 
 app.config.from_object(Config)
-
+print("HELLOOOOOO ")
 CORS(app,
-     origins=["https://mango-glacier-0805ada00.6.azurestaticapps.net"],
+     origins=["https://mango-glacier-0805ada00.6.azurestaticapps.net/",
+              "http://localhost:5173"],
      supports_credentials=True,
-     resources={
-         r"/*": {"origins": "https://mango-glacier-0805ada00.6.azurestaticapps.net"}},
+     resources={r"/*": {"origins": [
+         "https://mango-glacier-0805ada00.6.azurestaticapps.net/",
+         "http://localhost:5173"
+     ]}},
      methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
      allow_headers=["Content-Type", "Authorization"])
 
 jwt = JWTManager(app)
 
-@jwt.token_in_blocklist_loader
-def check_if_token_is_revoked(jwt_header, jwt_payload): 
-#  print("a",revoked_tokens)
- jti = jwt_payload["jti"]
- return jti in revoked_tokens
 
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload):
+    #  print("a",revoked_tokens)
+    jti = jwt_payload["jti"]
+    return jti in revoked_tokens
 
 
 db.init_app(app)
 init_models(db)
-migrate = Migrate(app,db)
-   
+migrate = Migrate(app, db)
+
 app.register_blueprint(user_bp)
 app.register_blueprint(product_category_bp)
 app.register_blueprint(product_bp)
@@ -47,7 +50,7 @@ app.register_blueprint(wishlist_bp)
 app.register_blueprint(shop_order)
 
 with app.app_context():
-        db.create_all() 
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(
